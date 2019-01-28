38b77742549f0fce365b91f7eab94568d474a6ec
Ahmed Samir <ahmed.samir.farid@gmail.com>
2019-01-28 14:21:53 +0200

    Initial commit

---
+++ src/components/ahmed.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="hello">
+    <h1>ahmed samir</h1>
+    <h2>Essential Links</h2>
+     <router-link to="/" exact>home</router-link>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ahmed',
+  
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
